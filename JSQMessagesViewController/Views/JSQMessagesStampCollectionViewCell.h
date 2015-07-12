6453b3a8166e511f689a84df608d0a995b73b9c0
usatie <usatie@gmail.com>
2015-07-12 15:18:35 +0900

    [add] JSQMessagesStampCollectionViewCell

---
+++ JSQMessagesViewController/Views/JSQMessagesStampCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  JSQMessagesStampCollectionViewCell.h
+//  JSQMessages
+//
+//  Created by Shun Usami on 2015/07/12.
+//  Copyright (c) 2015年 Hexed Bits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JSQMessagesStampCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ JSQMessagesViewController/Views/JSQMessagesStampCollectionViewCell.m
@@ -0,0 +1,17 @@
+//
+//  JSQMessagesStampCollectionViewCell.m
+//  JSQMessages
+//
+//  Created by Shun Usami on 2015/07/12.
+//  Copyright (c) 2015年 Hexed Bits. All rights reserved.
+//
+
+#import "JSQMessagesStampCollectionViewCell.h"
+
+@implementation JSQMessagesStampCollectionViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+@end
