d6938cad51e9e33552adf6a7cd8289010425f3bb
usatie <usatie@gmail.com>
2015-07-12 10:34:00 +0900

    [add] StampView

---
+++ JSQMessagesViewController/Views/JSQMessagesStampView.h
@@ -0,0 +1,13 @@
+//
+//  JSQMessagesStampView.h
+//  JSQMessages
+//
+//  Created by Shun Usami on 2015/07/12.
+//  Copyright (c) 2015年 Hexed Bits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JSQMessagesStampView : UIView
+
+@end
---
+++ JSQMessagesViewController/Views/JSQMessagesStampView.m
@@ -0,0 +1,21 @@
+//
+//  JSQMessagesStampView.m
+//  JSQMessages
+//
+//  Created by Shun Usami on 2015/07/12.
+//  Copyright (c) 2015年 Hexed Bits. All rights reserved.
+//
+
+#import "JSQMessagesStampView.h"
+
+@implementation JSQMessagesStampView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
